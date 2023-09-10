da07e12c4bd7c6bfa5edcfccb3db55402419e414
Jalon Duncan <duncanjalon@gmail.com>
2023-09-10 16:02:01 -0500

    finished TheBasics chapter

---
+++ TheBasics/Exercise10_11_12.js
@@ -0,0 +1,8 @@
+console.log("foo" === "Foo");
+//this returns false
+
+console.log(parseInt("3.1415"));
+//this returns 3
+
+console.log("12" < "9");
+// this returns true
---
+++ TheBasics/Exercise8_Ex9.js
@@ -0,0 +1,14 @@
+let names = ["asta", "butterscotch", "pudding", "neptune", "darwin", "bunny"];
+
+//console.log(names);
+
+let pets = {
+  asta: "dog",
+  butterscotch: "cat",
+  pudding: "cat",
+  neptune: "fish",
+  darwin: "lizard",
+  bunny: "bird",
+};
+
+//console.log(pets)
