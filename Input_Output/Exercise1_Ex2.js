ebea29a9b1f0ec7353505755821c84046dd88c23
Jalon Duncan <duncanjalon@gmail.com>
2023-09-19 12:12:55 -0500

    finished chapter in-out

---
+++ Input_Output/Exercise1_Ex2.js
@@ -0,0 +1,9 @@
+/* 
+let readlineSync = require("readline-sync");
+let name = readlineSync.question("What is your name? ");
+console.log(`Hello, ${name}!`);
+*/
+
+let firstName = prompt("What is your first name? ");
+let lastName = prompt("What is your last name?  ");
+console.log(`Hello, ${firstName} ${lastName}!`);
